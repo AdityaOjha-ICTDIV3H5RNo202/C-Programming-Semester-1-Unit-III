4b828fa1e858546bf45c41e98520a695a14d005f
Aditya Ojha <ojha8aditya@gmail.com>
2025-09-17 22:10:31 +0530

    Add files via upload
    This program prints first n even numbers.

---
+++ 1ST10EN.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int i,n,num;
+	printf("Enter total number of even numbers to be printed: ");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++)
+	{
+		printf("%d\n",num);
+		num+=2;
+	}
+	getch();
+}

d0acf685ec7989bcf64620be9094d123952f70b8
Aditya Ojha <ojha8aditya@gmail.com>
2025-09-17 20:35:19 +0530

    Add files via upload
    This program prints first ten natural numbers.

---
+++ 1ST10NN.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int i;
+	printf("The first ten natural numbers are:\n");
+	for (i=1;i<=10;i++)
+	{
+		printf("%d",i);
+		printf("\n");
+	}
+	getch();
+}

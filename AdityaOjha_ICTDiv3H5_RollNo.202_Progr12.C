084922b416241d647746ffd90af362cf7e01b09c
Aditya Ojha <ojha8aditya@gmail.com>
2025-09-18 15:30:49 +0530

    Create AdityaOjha_ICTDiv3H5_RollNo.202_Progr12.C

---
+++ AdityaOjha_ICTDiv3H5_RollNo.202_Progr12.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+        int i,n;
+        printf("Enter how many times you want to print my name: ");
+        scanf("%d",&n);
+        for (i=1;i<=n;i++)
+        {
+                printf("My name is Aditya Ojha.\n");
+        }
+}

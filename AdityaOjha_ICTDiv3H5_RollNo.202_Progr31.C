ed9ae149a2f42287915feadf5e47557877f9db88
Aditya Ojha <ojha8aditya@gmail.com>
2025-11-08 23:46:52 +0530

    Update AdityaOjha_ICTDiv3H5_RollNo.202_Progr31.C

--- AdityaOjha_ICTDiv3H5_RollNo.202_Progr31.C
+++ AdityaOjha_ICTDiv3H5_RollNo.202_Progr31.C
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, sum = 0, flag;
+    for(i = 2; i <= 500; i++) {
+        flag = 0;
+        for(j = 2; j <= i / 2; j++) {
+            if(i % j == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0)
+            sum += i;
+    }
+    printf("%d", sum);
+    return 0;
+}
 

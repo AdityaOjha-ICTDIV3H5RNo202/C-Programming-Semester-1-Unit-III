3c5a655d1e45dd1d64a299a491b5c42bb34d2c6a
Aditya Ojha <ojha8aditya@gmail.com>
2025-11-08 23:52:45 +0530

    Update and rename n.C to AdityaOjha_ICTDiv3H5_RollNo.202_Progr35.C

--- AdityaOjha_ICTDiv3H5_RollNo.202_Progr35.C
+++ AdityaOjha_ICTDiv3H5_RollNo.202_Progr35.C
@@ -3,7 +3,7 @@
 int main() {
     int i, j;
     for(i = 1; i <= 2; i++) {
-        for(j = 1; j <= 3; j++)
+        for(j = 1; j <= 3; j++) 
             printf("%d %d\n", i, j);
     }
     return 0;

0e289cd5943f070912c5bbcbdce8f2c8f369bac4
Aditya Ojha <ojha8aditya@gmail.com>
2025-11-08 23:47:48 +0530

    Update AdityaOjha_ICTDiv3H5_RollNo.202_Progr33.C

--- AdityaOjha_ICTDiv3H5_RollNo.202_Progr33.C
+++ AdityaOjha_ICTDiv3H5_RollNo.202_Progr33.C
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n, sq, temp, digits = 0, p = 1;
+    scanf("%d", &n);
+    sq = n * n;
+    temp = n;
+    while(temp > 0) {
+        digits++;
+        temp /= 10;
+    }
+    for(int i = 1; i <= digits; i++)
+        p *= 10;
+    if(sq % p == n)
+        printf("Automorphic number");
+    else
+        printf("Not automorphic number");
+    return 0;
+}
 
